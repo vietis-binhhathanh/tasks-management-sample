5d2e4e7cccfc5ca80d1b313e875336acae9c4d26
VietIS-Intern Bình <binh.hathanh@vietis.com.vn>
2020-03-04 21:32:23 +0700

    update

--- nestjs-task-management/src/main.ts
+++ nestjs-task-management/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import * as config from 'config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const serverConfig = config.get('server');
@@ -13,6 +14,15 @@ async function bootstrap() {
   // }
 
   const port = process.env.PORT || serverConfig.port;
+  const options = new DocumentBuilder()
+    .setTitle('Swagger example')
+    .setDescription('The swagger API description')
+    .setVersion('1.0')
+    .addTag('cats').build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port);
   logger.log(`Application running !!!`);
 }
